8104be5481c83aeef4f16df44b161f4d666c4d3b
Bravo1B9 <b1@xgmi.com>
2023-03-12 15:33:05 +0000

    Write some code that doesn't work

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hi' });
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+let users = [];
+
+app.post('/create', (req, res) => {
+  let newUser = { email, password };
+  newUser.email = req.body.email;
+  newUser.password = req.body.password;
+  users.push(newUser);
+});
+
+app.get('/get', (req, res) => {
+  res.send(users[0]);
 });
 
 app.listen(3000)
