6b51692078a06d929edfac1d2281c13ae76e9413
Bravo1B9 <b1@xgmi.com>
2023-03-13 06:02:04 +0000

    Do some stuff

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRouter = require('./routes/users');
+const req = require('express/lib/request');
 
 const app = express();
 
@@ -8,8 +9,25 @@ app.use('/users', userRouter);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+let users = [];
+
+class User {
+  constructor(email, password) {
+    this.email;
+    this.password;
+  }
+}
+
+app.post('/create', (req, res) => {
+  let email = req.body.emaill;
+  let password = req.body.password;
+  let newUser = new User(email, password);
+  users.push(newUser);
+  res.json(users[0]);
+});
+
 app.get('/get', (req, res) => {
-  res.json({ message: 'Hi' });
+  res.send(users[0]);
 });
 
 app.listen(3000);
